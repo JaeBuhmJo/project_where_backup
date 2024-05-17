b81d83bea76865cae063c197b75761ba9b68172c
JaeBuhmJo <favor99531@gmail.com>
2024-05-17 14:34:19 +0900

    환영모달 제작 및 재방문 체크 쿠키작업 완료

---
+++ share/modal_greeting.js
@@ -0,0 +1,38 @@
+// 모달 바깥 클릭하면 환영 모달 해제
+window.addEventListener("click", (e) => {
+  e.target.className == "modal_greeting" ? close_modal_greeting() : false;
+});
+
+function show_modal_greeting() {
+  document.getElementsByClassName("modal_greeting")[0].style.display = "inline-block";
+}
+function close_modal_greeting() {
+  document.getElementsByClassName("modal_greeting")[0].style.display = "none";
+}
+
+// 쿠키 확인
+const visitedCookie = getCookie("where_visited");
+if (!visitedCookie) {
+  // 쿠키가 존재하지 않는 경우
+  set_visited_cookie();
+  show_modal_greeting();
+}
+
+//오늘 이 사이트에 들어온 적이 없다면 쿠키 생성
+function set_visited_cookie() {
+  const cookieName = "where_visited";
+  const cookieValue = "1";
+
+  const date = new Date();
+  date.setTime(date.getTime() + 24 * 60 * 60 * 1000); // 24시간 * 60분 * 60초 * 1000밀리초
+
+  // 쿠키 설정
+  document.cookie = `${cookieName}=${cookieValue}; expires=${date.toUTCString()}; path=/`;
+}
+
+//쿠키를 확인해 오늘 이 사이트에 방문한 적이 있다면 환영 모달 해제
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
