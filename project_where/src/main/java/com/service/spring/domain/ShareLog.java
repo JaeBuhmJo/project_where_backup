a5a3c6706ecc744a28616d3ddaa1dd964f2be65a
JaeBuhmJo <favor99531@gmail.com>
2024-05-21 10:39:27 +0900

    랭킹 지하철 중심 수정

--- project_where/src/main/java/com/service/spring/domain/ShareLog.java
+++ project_where/src/main/java/com/service/spring/domain/ShareLog.java
@@ -1,31 +1,15 @@
 package com.service.spring.domain;
 
 public class ShareLog {
-	private int nodeId;
-	private String nodeName;
 	private int stationId;
 	private String stationName;
 	public ShareLog() {
 		super();
 	}
-	public ShareLog(int nodeId, String nodeName, int stationId, String stationName) {
+	public ShareLog(int stationId, String stationName) {
 		super();
-		this.nodeId = nodeId;
-		this.nodeName = nodeName;
 		this.stationId = stationId;
 		this.stationName = stationName;
-	}
-	public int getNodeId() {
-		return nodeId;
-	}
-	public void setNodeId(int nodeId) {
-		this.nodeId = nodeId;
-	}
-	public String getNodeName() {
-		return nodeName;
-	}
-	public void setNodeName(String nodeName) {
-		this.nodeName = nodeName;
 	}
 	public int getStationId() {
 		return stationId;
@@ -41,9 +25,7 @@ public class ShareLog {
 	}
 	@Override
 	public String toString() {
-		return "ShareLog [nodeId=" + nodeId + ", nodeName=" + nodeName + ", stationId=" + stationId + ", stationName="
-				+ stationName + "]";
+		return "ShareLog [stationId=" + stationId + ", stationName=" + stationName + "]";
 	}
-	
 	
 }
