584b8a6e95085300a44b1a051c4d6380010eb89d
student-jjh <wjswnsgur456@gmail.com>
2024-05-22 13:36:38 +0900

    위경도 해결

--- share/map.js
+++ share/map.js
@@ -11,10 +11,10 @@ var start = [];
 
 // 지도를 생성합니다
 var mapContainer = document.getElementById("map"),
-  mapOption = {
-    center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
-    level: 7, // 지도의 확대 레벨
-  };
+    mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567), // 지도의 중심좌표
+        level: 7, // 지도의 확대 레벨
+    };
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
 // 주소로 좌표 반환 객체
@@ -23,129 +23,130 @@ var geocoder = new kakao.maps.services.Geocoder();
 
 // 출발인원 조정 +
 function addFun(e) {
-  let temp_count = 0;
-  for (const [key, value] of Object.entries(marker_dict)) {
-    temp_count += value["count"] * 1;
-  }
+    let temp_count = 0;
+    for (const [key, value] of Object.entries(marker_dict)) {
+        temp_count += value["count"] * 1;
+    }
 
-  if (temp_count >= 16) {
-    toast_alert("최대 16명 까지 추가 할 수 있습니다.", 2000);
-    return;
-  }
-  marker_dict[e.target.value]["count"] += 1;
-  e.target.parentElement.querySelector(".count").innerHTML = e.target.parentElement.querySelector(".count").innerHTML * 1 + 1;
+    if (temp_count >= 16) {
+        toast_alert("최대 16명 까지 추가 할 수 있습니다.", 2000);
+        return;
+    }
+    marker_dict[e.target.value]["count"] += 1;
+    e.target.parentElement.querySelector(".count").innerHTML = e.target.parentElement.querySelector(".count").innerHTML * 1 + 1;
 }
 
 // 출발인원 조정 -
 function minFun(e) {
-  if (e.target.parentElement.querySelector(".count").innerHTML * 1 > 1) {
-    marker_dict[e.target.value]["count"] -= 1;
-    e.target.parentElement.querySelector(".count").innerHTML = e.target.parentElement.querySelector(".count").innerHTML * 1 - 1;
-  }
+    if (e.target.parentElement.querySelector(".count").innerHTML * 1 > 1) {
+        marker_dict[e.target.value]["count"] -= 1;
+        e.target.parentElement.querySelector(".count").innerHTML = e.target.parentElement.querySelector(".count").innerHTML * 1 - 1;
+    }
 }
 
 // 출발인원 삭제
 function delFun(e) {
-  const key_for_del = e.target.value;
-  e.target.parentElement.remove();
-  marker_dict[key_for_del]["marker"].setMap(null);
-  delete marker_dict[key_for_del];
-  set_pos_guide();
+    const key_for_del = e.target.value;
+    e.target.parentElement.remove();
+    marker_dict[key_for_del]["marker"].setMap(null);
+    delete marker_dict[key_for_del];
+    set_pos_guide();
 }
 
 
 
 // 지도 클릭 시 마커 생성
 kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-  let temp_count = 0;
-  for (const [key, value] of Object.entries(marker_dict)) {
-    temp_count += value["count"] * 1;
-  }
-  if (temp_count >= 16) {
-    toast_alert("최대 16명 까지 추가 할 수 있습니다.", 2000);
-    return;
-  }
-
-  // 지도에 마커를 표시합니다
-  var imageSrc = "./img/pin_green.png";
-  var imageSize = new kakao.maps.Size(23.33, 30);
-  var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-  var marker = new kakao.maps.Marker({
-    image: markerImage,
-  });
-
-  marker.setDraggable(true); 
-  kakao.maps.event.addListener(marker, 'dragstart', function() {
-    // 출발 마커의 드래그가 시작될 때 마커 이미지를 변경합니다
-    window.key_for_change = marker.getPosition().Ma+"," + marker.getPosition().La
-
-});
-
-  kakao.maps.event.addListener(marker, 'dragend', function() {
-    
-   const temp_count = marker_dict[window.key_for_change]["count"]
-    
-   delete marker_dict[window.key_for_change];
-
-   const key = marker.getPosition().Ma+"," + marker.getPosition().La
-
-   marker_dict[key] = {
-    marker: marker,
-    count: temp_count,
-  };
-  let temp_start_pos = document.getElementById(window.key_for_change)
-  temp_start_pos.setAttribute('id',key)
-
-  let temp_plus_button = temp_start_pos.querySelector(".plus_button")
-  let temp_min_button = temp_start_pos.querySelector(".minus_button")
-  let temp_delete_button = temp_start_pos.querySelector(".delete_button")
-
-  temp_plus_button.setAttribute('value',key)
-  temp_min_button.setAttribute('value',key)
-  temp_delete_button.setAttribute('value',key)
-  window.location2 = ""
-  getAddr(marker.getPosition().getLat(),marker.getPosition().getLng());
-
-  setTimeout(function(){
-    if(window.location2 != ""){
-      let loc = temp_start_pos.querySelector(".loc")
-      loc.innerHTML = window.location2
+    let temp_count = 0;
+    for (const [key, value] of Object.entries(marker_dict)) {
+        temp_count += value["count"] * 1;
     }
-  },200)
+    if (temp_count >= 16) {
+        toast_alert("최대 16명 까지 추가 할 수 있습니다.", 2000);
+        return;
+    }
 
-   
-  });
+    // 지도에 마커를 표시합니다
+    var imageSrc = "./img/pin_green.png";
+    var imageSize = new kakao.maps.Size(23.33, 30);
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+    var marker = new kakao.maps.Marker({
+        image: markerImage,
+    });
 
-  // 클릭한 위도, 경도 정보를 가져옵니다
-  var latlng = mouseEvent.latLng;
+    marker.setDraggable(true);
+    kakao.maps.event.addListener(marker, 'dragstart', function () {
+        // 출발 마커의 드래그가 시작될 때 마커 이미지를 변경합니다
+        window.key_for_change = marker.getPosition().getLat() + "," + marker.getPosition().getLng()
+        console.log(1)
+        console.log(window.key_for_change)
+    });
 
-  // 마커 위치를 클릭한 위치로 옮깁니다
-  marker.setPosition(latlng);
-  // console.log(latlng);
-  // 도보 출발지 배열에 마커 위치를 추가합니다.
-  start.push([latlng.getLat(),latlng.getLng()]);
+    kakao.maps.event.addListener(marker, 'dragend', function () {
 
-  const key = latlng.getLat() + "," + latlng.getLng();
-  if (key in marker_dict) {
-    return;
-  }
+        let temp_count = marker_dict[window.key_for_change]["count"]
 
- 
+        delete marker_dict[window.key_for_change];
 
-  // 선택창(start_pos_list) 등록
-  const start_pos_list = document.getElementById("start_pos_list");
-  getAddr(latlng.getLat(), latlng.getLng());
-  let for_html = "";
-  setTimeout(function () {
-    if (window.location2 != ""){
-      marker_dict[key] = {
-        marker: marker,
-        count: 1,
-      };
-      let newDiv = document.createElement("div");
-      newDiv.setAttribute("class", "start_pos");
-      newDiv.setAttribute('id',key)
-      for_html += `
+        let key = marker.getPosition().getLat() + "," + marker.getPosition().getLng()
+
+        marker_dict[key] = {
+            marker: marker,
+            count: temp_count,
+        };
+        let temp_start_pos = document.getElementById(window.key_for_change)
+        temp_start_pos.setAttribute('id', key)
+
+        let temp_plus_button = temp_start_pos.querySelector(".plus_button")
+        let temp_min_button = temp_start_pos.querySelector(".minus_button")
+        let temp_delete_button = temp_start_pos.querySelector(".delete_button")
+
+        temp_plus_button.setAttribute('value', key)
+        temp_min_button.setAttribute('value', key)
+        temp_delete_button.setAttribute('value', key)
+        window.location2 = ""
+        getAddr(marker.getPosition().getLat(), marker.getPosition().getLng());
+
+        setTimeout(function () {
+            if (window.location2 != "") {
+                let loc = temp_start_pos.querySelector(".loc")
+                loc.innerHTML = window.location2
+            }
+        }, 200)
+
+
+    });
+
+    // 클릭한 위도, 경도 정보를 가져옵니다
+    var latlng = mouseEvent.latLng;
+
+    // 마커 위치를 클릭한 위치로 옮깁니다
+    marker.setPosition(latlng);
+    // console.log(latlng);
+    // 도보 출발지 배열에 마커 위치를 추가합니다.
+    start.push([latlng.getLat(), latlng.getLng()]);
+
+    const key = latlng.getLat() + "," + latlng.getLng();
+    if (key in marker_dict) {
+        return;
+    }
+
+
+
+    // 선택창(start_pos_list) 등록
+    const start_pos_list = document.getElementById("start_pos_list");
+    getAddr(latlng.getLat(), latlng.getLng());
+    let for_html = "";
+    setTimeout(function () {
+        if (window.location2 != "") {
+            marker_dict[key] = {
+                marker: marker,
+                count: 1,
+            };
+            let newDiv = document.createElement("div");
+            newDiv.setAttribute("class", "start_pos");
+            newDiv.setAttribute('id', key)
+            for_html += `
           <span class="pos_img"></span>   
           <span class="loc">${window.location2}</span>
 
@@ -158,58 +159,58 @@ kakao.maps.event.addListener(map, "click", function (mouseEvent) {
           <button type="button" class="delete_button" value ="${key}"></button>
           `;
 
-      newDiv.innerHTML += for_html;
-      newDiv.querySelector(".plus_button").addEventListener("click", addFun);
-      newDiv.querySelector(".minus_button").addEventListener("click", minFun);
-      newDiv.querySelector(".delete_button").addEventListener("click", delFun);
-      start_pos_list.appendChild(newDiv);
-      // 지도에 마커 등록
-      marker.setMap(map);
-      set_pos_guide()
-      const menu_div = document.getElementById("menu");
-      menu_div.scrollTop = menu_div.scrollHeight;
-    }else{
-      toast_alert("올바른 위치를 클릭해주세요.", 2000);
-    }
-    
-  }, 80);
+            newDiv.innerHTML += for_html;
+            newDiv.querySelector(".plus_button").addEventListener("click", addFun);
+            newDiv.querySelector(".minus_button").addEventListener("click", minFun);
+            newDiv.querySelector(".delete_button").addEventListener("click", delFun);
+            start_pos_list.appendChild(newDiv);
+            // 지도에 마커 등록
+            marker.setMap(map);
+            set_pos_guide()
+            const menu_div = document.getElementById("menu");
+            menu_div.scrollTop = menu_div.scrollHeight;
+        } else {
+            toast_alert("올바른 위치를 클릭해주세요.", 2000);
+        }
+
+    }, 80);
 
 });
 
 // locatation = 자연어 주소
 let location2 = "";
 function getAddr(lat, lng) {
-  window.location2 = "";
-  let geocoder = new kakao.maps.services.Geocoder();
+    window.location2 = "";
+    let geocoder = new kakao.maps.services.Geocoder();
 
-  let coord = new kakao.maps.LatLng(lat, lng);
-  let callback = function (result, status) {
-    if (status === kakao.maps.services.Status.OK) {
-      window.location2 = result[0].address.address_name;
+    let coord = new kakao.maps.LatLng(lat, lng);
+    let callback = function (result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            window.location2 = result[0].address.address_name;
 
-    }
-  };
-  geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-  return window.location2;
+        }
+    };
+    geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
+    return window.location2;
 }
 
 // 초기화
 const clear_fun = () => {
-  for (const [key, value] of Object.entries(marker_dict)) {
-    value["marker"].setMap(null);  
-  }
-  // 선택창 초기화
-  clear_start_pos_list();
-  marker_for_middle.setMap(null);
-  marker_dict = {};
-  set_pos_guide();
-  start = [];
-        
+    for (const [key, value] of Object.entries(marker_dict)) {
+        value["marker"].setMap(null);
+    }
+    // 선택창 초기화
+    clear_start_pos_list();
+    marker_for_middle.setMap(null);
+    marker_dict = {};
+    set_pos_guide();
+    start = [];
+
 };
 
 const clear_start_pos_list = () => {
-  const start_pos_list = document.getElementById("start_pos_list");
-  start_pos_list.innerHTML = "";
+    const start_pos_list = document.getElementById("start_pos_list");
+    start_pos_list.innerHTML = "";
 };
 
 // 초기화 클릭 이벤트
@@ -218,12 +219,12 @@ clear.addEventListener("click", clear_fun);
 
 
 // 출발지 리스트 초기 가이드(지도에서 출발지를 선택해보세요!)
-function set_pos_guide(){
+function set_pos_guide() {
 
-  const start_guide = document.getElementById("default_start_guide");
-  if(Object.keys(marker_dict).length === 0){
-      start_guide.style.display = "block";
-  }else{
-      start_guide.style.display = "none";
-  }
+    const start_guide = document.getElementById("default_start_guide");
+    if (Object.keys(marker_dict).length === 0) {
+        start_guide.style.display = "block";
+    } else {
+        start_guide.style.display = "none";
+    }
 }
--- share/url.js
+++ share/url.js
@@ -1,5 +1,4 @@
 function getAddr_for_url(lat, lng) {
-
   return new Promise((resolve, reject) => {
     let geocoder = new kakao.maps.services.Geocoder();
     let coord = new kakao.maps.LatLng(lat, lng);
@@ -9,7 +8,7 @@ function getAddr_for_url(lat, lng) {
         let addr = result[0].address.address_name;
         resolve(addr);
       } else {
-        reject(new Error('Failed to get address'));
+        reject(new Error("Failed to get address"));
       }
     };
 
@@ -17,99 +16,94 @@ function getAddr_for_url(lat, lng) {
   });
 }
 
-function ajax_for_url(key){
-  return new Promise((resolve,reject) => {
+function ajax_for_url(key) {
+  return new Promise((resolve, reject) => {
     $.ajax({
       async: true,
       type: "get",
-      url: "http://127.0.0.1:9000/sharelog/"+key,
+      url: "http://127.0.0.1:9000/sharelog/" + key,
 
       success: function (data) {
-          resolve(data);
+        resolve(data);
       },
+    });
   });
-  })
 }
 
+if (window.location.href != "http://127.0.0.1:5500/index.html") {
+  let info = window.location.href;
+  key = info.replace("http://127.0.0.1:5500/index.html?uuid=", "");
 
-if (window.location.href != "http://127.0.0.1:5500/index.html"){
-    let info = window.location.href
-    key = info.replace("http://127.0.0.1:5500/index.html?uuid=","")
+  const temp_fun = async (for_marker) => {
+    console.log("forMarker");
+    console.log(for_marker);
+    for (infos of for_marker) {
+      let temp_info = infos.split("&");
 
+      let key = temp_info[0] + "," + temp_info[1];
 
-    const temp_fun = async (for_marker) => {
-      console.log("forMarker")
-      console.log(for_marker)
-      for (infos of for_marker){
-        let temp_info = infos.split("&");
+      getAddr_for_url(temp_info[0], temp_info[1]).then((addr) => {
+        // 지도에 마커를 표시합니다
+        var imageSrc = "./img/pin_green.png";
+        var imageSize = new kakao.maps.Size(23.33, 30);
+        var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+        var marker = new kakao.maps.Marker({
+          image: markerImage,
+        });
 
-        let key = temp_info[0] + "," + temp_info[1] 
+        marker.setDraggable(true);
+        kakao.maps.event.addListener(marker, "dragstart", function () {
+          // 출발 마커의 드래그가 시작될 때 마커 이미지를 변경합니다
+          window.key_for_change = marker.getPosition().getLat() + "," + marker.getPosition().getLng()
+          console.log(window.key_for_change)
+        });
 
-        getAddr_for_url(temp_info[0],temp_info[1]).then(addr =>{
-          // 지도에 마커를 표시합니다
-  var imageSrc = "./img/pin_green.png";
-  var imageSize = new kakao.maps.Size(23.33, 30);
-  var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-  var marker = new kakao.maps.Marker({
-    image: markerImage,
-  });
+        kakao.maps.event.addListener(marker, "dragend", function () {
+          setTimeout(function () {}, 70);
+          const temp_count = marker_dict[window.key_for_change]["count"];
 
-  marker.setDraggable(true); 
-  kakao.maps.event.addListener(marker, 'dragstart', function() {
-    // 출발 마커의 드래그가 시작될 때 마커 이미지를 변경합니다
-    window.key_for_change = marker.getPosition().Ma+"," + marker.getPosition().La
+          delete marker_dict[window.key_for_change];
 
-});
+          const key = marker.getPosition().getLat() + "," + marker.getPosition().getLng()
 
-  kakao.maps.event.addListener(marker, 'dragend', function() {
-    
-   const temp_count = marker_dict[window.key_for_change]["count"]
-    
-   delete marker_dict[window.key_for_change];
+          marker_dict[key] = {
+            marker: marker,
+            count: temp_count,
+          };
+          let temp_start_pos = document.getElementById(window.key_for_change);
+          temp_start_pos.setAttribute("id", key);
 
-   const key = marker.getPosition().Ma+"," + marker.getPosition().La
+          let temp_plus_button = temp_start_pos.querySelector(".plus_button");
+          let temp_min_button = temp_start_pos.querySelector(".minus_button");
+          let temp_delete_button = temp_start_pos.querySelector(".delete_button");
 
-   marker_dict[key] = {
-    marker: marker,
-    count: temp_count,
-  };
-  let temp_start_pos = document.getElementById(window.key_for_change)
-  temp_start_pos.setAttribute('id',key)
+          temp_plus_button.setAttribute("value", key);
+          temp_min_button.setAttribute("value", key);
+          temp_delete_button.setAttribute("value", key);
+          window.location2 = "";
+          getAddr(marker.getPosition().getLat(), marker.getPosition().getLng());
 
-  let temp_plus_button = temp_start_pos.querySelector(".plus_button")
-  let temp_min_button = temp_start_pos.querySelector(".minus_button")
-  let temp_delete_button = temp_start_pos.querySelector(".delete_button")
+          setTimeout(function () {
+            if (window.location2 != "") {
+              let loc = temp_start_pos.querySelector(".loc");
+              loc.innerHTML = window.location2;
+            }
+          }, 200);
+        });
 
-  temp_plus_button.setAttribute('value',key)
-  temp_min_button.setAttribute('value',key)
-  temp_delete_button.setAttribute('value',key)
-  window.location2 = ""
-  getAddr(marker.getPosition().getLat(),marker.getPosition().getLng());
+        var latlng = new kakao.maps.LatLng(temp_info[0], temp_info[1]);
+        marker.setPosition(latlng);
 
-  setTimeout(function(){
-    if(window.location2 != ""){
-      let loc = temp_start_pos.querySelector(".loc")
-      loc.innerHTML = window.location2
-    }
-  },200)
-
-   
-  });
-
-          var latlng = new kakao.maps.LatLng(temp_info[0], temp_info[1]);
-          marker.setPosition(latlng);
-
-
-          let for_html = "";
+        let for_html = "";
         setTimeout(function () {
-          if (addr != ""){
+          if (addr != "") {
             marker_dict[key] = {
               marker: marker,
               count: temp_info[2],
             };
             let newDiv = document.createElement("div");
             newDiv.setAttribute("class", "start_pos");
-            newDiv.setAttribute('id',key)
+            newDiv.setAttribute("id", key);
             for_html += `
                 <span class="pos_img"></span>   
                 <span class="loc">${addr}</span>
@@ -122,7 +116,7 @@ if (window.location.href != "http://127.0.0.1:5500/index.html"){
                 
                 <button type="button" class="delete_button" value ="${key}"></button>
                 `;
-      
+
             newDiv.innerHTML += for_html;
             newDiv.querySelector(".plus_button").addEventListener("click", addFun);
             newDiv.querySelector(".minus_button").addEventListener("click", minFun);
@@ -130,46 +124,32 @@ if (window.location.href != "http://127.0.0.1:5500/index.html"){
             start_pos_list.appendChild(newDiv);
             // 지도에 마커 등록
             marker.setMap(map);
-            set_pos_guide()
+            set_pos_guide();
             const menu_div = document.getElementById("menu");
             menu_div.scrollTop = menu_div.scrollHeight;
-          }else{
+          } else {
             toast_alert("올바른 위치를 클릭해주세요.", 1000);
           }
-          window.location2 =""
+          window.location2 = "";
         }, 300);
-        })
-
-
-        
-      }
+      });
     }
-    const url_wait = async () =>{
-     
-      ajax_for_url(key).then(data =>{
-        console.log(data)
-        data = data.replace("http://127.0.0.1:5500/index.html?","")
-        let for_marker = data.split('|');
-        const start_pos_list = document.getElementById("start_pos_list");
-        temp_fun(for_marker)
-      })
-    }
-    url_wait()
-    setTimeout(function(){
-      const find = document.getElementById("find_btn")
-      find.click();
-    },700)
-    
+  };
+  const url_wait = async () => {
+    ajax_for_url(key).then((data) => {
+      console.log(data);
+      data = data.replace("http://127.0.0.1:5500/index.html?", "");
+      let for_marker = data.split("|");
+      const start_pos_list = document.getElementById("start_pos_list");
+      temp_fun(for_marker);
+    });
+  };
+  url_wait();
+  setTimeout(function () {
+    const find = document.getElementById("find_btn");
+    find.click();
+  }, 700);
 
-
-    history.pushState(null, null, 'http://127.0.0.1:5500/index.html')
-    
-    
-    
-    
-
-    
+  history.pushState(null, null, "http://127.0.0.1:5500/index.html");
 }
 // alert(window.location.href)
-
-
